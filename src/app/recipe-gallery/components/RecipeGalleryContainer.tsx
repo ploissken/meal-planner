d79842678232426d26c542be269470638a49d9c8
rodurico <ploissken@gmail.com>
2025-09-26 19:12:41 -0300

    adding skeletons

--- src/app/recipe-gallery/components/RecipeGalleryContainer.tsx
+++ src/app/recipe-gallery/components/RecipeGalleryContainer.tsx
@@ -3,17 +3,24 @@ import { Container, Typography, Grid } from "@mui/material";
 import { RecipeCard } from "./RecipeCard";
 import { mockFetch } from "@/mockFetch";
 import { useEffect, useState } from "react";
-import recipes from "../../../mock-data/recipes.json";
+import mockRecipes from "../../../mock-data/recipes.json";
 import { Recipe } from "@/types";
+import RecipeCardSkeleton from "./RecipeCardSkeleton";
 
 export default function RecipeGalleryContainer() {
   // TODO: useContext
   const [recipes, setRecipes] = useState<Array<Recipe>>([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
-    mockFetch<Array<Recipe>>(recipes as Array<Recipe>, 1500).then((data) => {
-      console.log("fetched data", data);
-      setRecipes(data);
-    });
+    setLoading(true);
+    mockFetch<Array<Recipe>>(mockRecipes as Array<Recipe>, 1500).then(
+      (data) => {
+        console.log("fetched data", data);
+        setRecipes(data);
+        setLoading(false);
+      }
+    );
   }, []);
 
   return (
@@ -27,11 +34,18 @@ export default function RecipeGalleryContainer() {
         spacing={{ xs: 2, md: 3 }}
         columns={{ xs: 4, sm: 8, md: 12 }}
       >
-        {[...Array(20)].map((_, i) => (
-          <Grid key={i} size={{ xs: 4, sm: 4, md: 4, lg: 3 }}>
-            <RecipeCard key={i} recipe={recipes?.[0]} />
-          </Grid>
-        ))}
+        {loading &&
+          [...Array(20)].map((_, i) => (
+            <Grid key={i} size={{ xs: 4, sm: 4, md: 4, lg: 3 }}>
+              <RecipeCardSkeleton />
+            </Grid>
+          ))}
+        {recipes.length > 0 &&
+          [...Array(20)].map((_, i) => (
+            <Grid key={i} size={{ xs: 4, sm: 4, md: 4, lg: 3 }}>
+              <RecipeCard key={i} recipe={recipes?.[0]} />
+            </Grid>
+          ))}
       </Grid>
     </Container>
   );
